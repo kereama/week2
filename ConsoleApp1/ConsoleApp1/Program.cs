26f027b341c7b89c1e6c69bf45db4749f6787783
Kereama Teua <30013220@student.toiohomai.ac.nz>
2018-08-01 17:12:39 +1200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, please enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", input);
+
+            Console.WriteLine("what is your age: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Do you have a Raho or a Teme?: ");
+            string input2 = Console.ReadLine();
+
+            Console.WriteLine("kei hea to Kainga?: ");
+            string input3 = Console.ReadLine();
+
+            Console.WriteLine("the information gathered: ");
+            Console.WriteLine("Name: {0}", input);
+            Console.WriteLine("Number: {0}", number);
+            Console.WriteLine("Gender: {0}", input2);
+            Console.WriteLine("Kainga: {0}", input3);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
