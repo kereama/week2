852895190399a9a5aed978585ff2c6309d048491
Kereama Teua <30013220@student.toiohomai.ac.nz>
2018-08-02 14:46:20 +1200

    third comit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,24 +10,56 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, please enter your name: ");
-            string input = Console.ReadLine();
-            Console.WriteLine("Hello, {0}", input);
+            //Console.WriteLine("hello, please enter your name: ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Hello, {0}", input);
 
-            Console.WriteLine("what is your age: ");
-            int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("what is your age: ");
+            //int number = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Do you have a Raho or a Teme?: ");
-            string input2 = Console.ReadLine();
+            //Console.WriteLine("Do you have a Raho or a Teme?: ");
+            //string input2 = Console.ReadLine();
 
-            Console.WriteLine("kei hea to Kainga?: ");
-            string input3 = Console.ReadLine();
+            //Console.WriteLine("kei hea to Kainga?: ");
+            //string input3 = Console.ReadLine();
 
-            Console.WriteLine("the information gathered: ");
-            Console.WriteLine("Name: {0}", input);
-            Console.WriteLine("Number: {0}", number);
-            Console.WriteLine("Gender: {0}", input2);
-            Console.WriteLine("Kainga: {0}", input3);
+            //Console.WriteLine("the information gathered: ");
+            //Console.WriteLine("Name: {0}", input);
+            //Console.WriteLine("Number: {0}", number);
+            //Console.WriteLine("Gender: {0}", input2);
+            //Console.WriteLine("Kainga: {0}", input3);
+
+            //task2ask the user for two numbers
+            //Console.WriteLine("Please enter a number: ");
+            //int Num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter another number: ");
+            //int Num2 = int.Parse(Console.ReadLine());
+            ////add two numbers together
+            //Console.WriteLine("{0} + {1} = {2}", Num1, Num2, (Num1 + Num2));
+
+            //// subtraction
+            //Console.WriteLine("{0} - {1} = {2}", Num1, Num2, (Num1 - Num2));
+
+            ////multiply 
+            //Console.WriteLine("{0} * {1} = {2}", Num1, Num2, (Num1 * Num2));
+
+            ////devide 
+            //Console.WriteLine("{0} / {1} = {2}", Num1, Num2, (Num1 / Num2));
+
+            //Console.WriteLine("Task two complete - please press enter to complete");
+            //Console.ReadLine();
+
+            // task three. ask for users first and last name then display them together
+            Console.WriteLine("task3 He aha to ingoa tuatahi: ");
+            string fname = Console.ReadLine();
+            Console.WriteLine("he aha to ingoa whanau: ");
+            string lname = Console.ReadLine();
+            // Display the full name together 
+            Console.WriteLine("Tena koe {0} {1}", fname, lname);
+
+            Console.WriteLine("Kua mutu, Nga mihi");
+            Console.ReadLine(); 
+
 
 
             Console.ReadLine();
